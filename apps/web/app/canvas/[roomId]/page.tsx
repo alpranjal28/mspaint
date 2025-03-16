31c0925d4783162566beb130d378f0598aedc248
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2025-03-16 22:05:38 +0530

    added rect

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -7,15 +7,39 @@ export default function Canvas() {
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const context = canvas.getContext("2d");
-    if (!context) return;
-    context.fillStyle = "red";
-    context.clearRect(0, 0, 500, 500);
-    context.fillRect(60, 320, 100, 100);
-  })
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    // stroke
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+
+    // rect
+    let clicked = false;
+    let startX = 0;
+    let startY = 0;
+    canvas.addEventListener("mousedown", (e) => {
+      clicked = true;
+      startX = e.clientX;
+      startY = e.clientY;
+    });
+    canvas.addEventListener("mouseup", (e) => {
+      clicked = false;
+    });
+    canvas.addEventListener("mousemove", (e) => {
+      if (clicked) {
+        const width = e.clientX - startX;
+        const height = e.clientY - startY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeRect(startX, startY, width, height);
+      }
+    });
+  });
   return (
     <main>
-      <canvas  height={500} ref={canvasRef} width={500}></canvas>
+      <canvas height={500} ref={canvasRef} width={500}></canvas>
     </main>
   );
 }
