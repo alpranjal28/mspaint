755f206ca6c9896dd3304820a185ad6738e754f7
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2025-03-17 23:37:19 +0530

    working state

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,45 +1,38 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Draw } from "../../../draw";
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const [windowDimensions, setWindowDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({ width: window.innerWidth, height: window.innerHeight });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    // stroke
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
-
-    // rect
-    let clicked = false;
-    let startX = 0;
-    let startY = 0;
-    canvas.addEventListener("mousedown", (e) => {
-      clicked = true;
-      startX = e.clientX;
-      startY = e.clientY;
-    });
-    canvas.addEventListener("mouseup", (e) => {
-      clicked = false;
-    });
-    canvas.addEventListener("mousemove", (e) => {
-      if (clicked) {
-        const width = e.clientX - startX;
-        const height = e.clientY - startY;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeRect(startX, startY, width, height);
-      }
-    });
+    Draw(canvas);
   });
   return (
-    <main>
-      <canvas height={500} ref={canvasRef} width={500}></canvas>
+    <main className="flex min-h-screen bg-gray-300">
+      <canvas
+        height={windowDimensions.height}
+        ref={canvasRef}
+        width={windowDimensions.width}
+      ></canvas>
+      <div className="absolute top-0 left-0 right-0 flex justify-center gap-4 p-4 ">
+        <div className="border-2 border-black p-2 bg-red-300">rect</div>
+        <div className="border-2 border-black p-2 bg-red-300 rounded-full">circle</div>
+      </div>
     </main>
   );
 }
---
+++ apps/web/draw/index.ts
@@ -0,0 +1,79 @@
+type Shapes =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      x: number;
+      y: number;
+      radius: number;
+    };
+
+let existingShapes: Shapes[] = [];
+
+export function Draw(canvas: HTMLCanvasElement) {
+  // const canvas = canvasRef.current;
+  if (!canvas) return;
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+
+  // stroke
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+
+  // rect
+  let clicked = false;
+  let startX = 0;
+  let startY = 0;
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    startX = e.clientX;
+    startY = e.clientY;
+  });
+  canvas.addEventListener("mouseup", (e) => {
+    clicked = false;
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      width,
+      height,
+    });
+  });
+  canvas.addEventListener("mousemove", (e) => {
+    if (clicked) {
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      renderCanvas(ctx, canvas);
+      ctx.strokeStyle = "red";
+      ctx.strokeRect(startX, startY, width, height);
+      ctx.beginPath();
+      ctx.arc(100, 100, 80, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  });
+}
+
+function renderCanvas(
+  ctx: CanvasRenderingContext2D,
+  canvas: HTMLCanvasElement
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    }
+    if (shape.type === "circle") {
+      ctx.arc(shape.x, shape.y, shape.radius, shape.x + 10, shape.y + 10);
+    }
+  });
+}
