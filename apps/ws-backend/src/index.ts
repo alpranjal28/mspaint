0acead63f3dbaf7fa347fc117208f11e454e64cb
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2025-05-15 14:59:17 +0530

    reverted ws to prev commit - broke broadcast

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -6,6 +6,13 @@ import { prismaClient } from "@repo/db-config/prisma";
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+// const rooms: Map<string, Set<WebSocket>> = new Map();
+// const users: Map<string, WebSocket> = new Map();
+
+// const broadcast = (message: string) => {
+//   users.forEach((ws) => ws.send(message));
+// };
+
 interface RoomUser {
   ws: WebSocket;
   room: number[];
@@ -69,13 +76,18 @@ wss.on("connection", (ws, request) => {
         },
       });
 
-      // Only broadcast to users in the same room
+      // console.log("room users", roomUsers);
+
       roomUsers.forEach((u) => {
+        console.log("broadcasting to room", roomId);
+        u.ws.send(
+          JSON.stringify({ type: "broadcasted", message, roomId, userId })
+        );
+
         if (u.room.includes(roomId)) {
-          u.ws.send(
-            JSON.stringify({ type: "broadcasted", message, roomId, userId })
-          );
           console.log("broadcasted to room", roomId);
+        } else {
+          console.log("not broadcasting to room", roomId);
         }
       });
     }
